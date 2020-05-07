ab930daa12974806b5861d733cbd8dd75b5aaa27
emelyaraujosr <emelyaraujosr@gmail.com>
2020-05-07 18:12:42 -0300

    Criação da area administrativa, apresentação dos dados no DB no tamplete e criação do arquivo static

--- Site_ProjetoWeb/core/views.py
+++ Site_ProjetoWeb/core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def index(request):
+    listaDeProdutos = Product.objects.all()
+    context = {
+            'produtos': listaDeProdutos
+    }
+    return render (request, 'index.html', context)
